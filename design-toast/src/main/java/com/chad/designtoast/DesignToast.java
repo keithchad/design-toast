aceef78955e064eaba92e1ece7f07ab03e1fd56e
Chad <keithchad10@outlook.com>
2021-01-23 16:46:23 +0300

    modifying sample app

--- design-toast/src/main/java/com/chad/designtoast/DesignToast.java
+++ design-toast/src/main/java/com/chad/designtoast/DesignToast.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
--- sample/src/main/java/com/chad/customtoast/MainActivity.java
+++ sample/src/main/java/com/chad/customtoast/MainActivity.java
@@ -1,9 +1,10 @@
 package com.chad.customtoast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.chad.designtoast.DesignToast;
 
@@ -13,6 +14,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DesignToast.makeText(this, "Hello", Toast.LENGTH_SHORT, DesignToast.TYPE_ERROR).show();
+        initialize();
     }
+
+    private void initialize() {
+        Button buttonSuccess = findViewById(R.id.buttonSuccess);
+        Button buttonWarning = findViewById(R.id.buttonWarning);
+        Button buttonError = findViewById(R.id.buttonError);
+        Button buttonInfo = findViewById(R.id.buttonInfo);
+
+        buttonSuccess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSuccessToast();
+            }
+        });
+
+        buttonWarning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showWarningToast();
+            }
+        });
+
+        buttonError.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showErrorToast();
+            }
+        });
+
+        buttonInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInfoToast();
+            }
+        });
+
+    }
+
+    private void showSuccessToast() {
+        DesignToast.makeText(this, "SUCCESS", DesignToast.TYPE_SUCCESS).show();
+    }
+
+    private void showWarningToast() {
+        DesignToast.makeText(this, "WARNING", DesignToast.TYPE_WARNING).show();
+    }
+
+    private void showErrorToast() {
+        DesignToast.makeText(this, "ERROR", DesignToast.TYPE_ERROR).show();
+    }
+
+    private void showInfoToast() {
+        DesignToast.makeText(this, "INFO", DesignToast.TYPE_INFO).show();
+    }
+
 }
